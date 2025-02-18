8b1c950ec7626df6e3ca4ff49222048ae39770f6
MizzenMastodon <Nbaum0701@gmail.com>
2025-02-18 00:54:04 -0500

    Update Chapter-2-Projects.cpp

--- Chapter-2-Projects.cpp
+++ Chapter-2-Projects.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
   int polledCustomers = 16500;
-  double percentPurchased = polledCustomers * 0.15; 
-  double percentCitrus = percentPurchased * 0.58; //percentage of citrus purchasing customers
+  int percentPurchased = polledCustomers * 0.15; 
+  int percentCitrus = percentPurchased * 0.58; //percentage of citrus purchasing customers
 
   cout << "Out of " << polledCustomers << " polled customers, " << percentPurchased << " purchased one or more energy drinks per week. " << percentCitrus << " of those customers purchased citrus-flavored energy drinks. "; 
 }
