37e3b6269f930536691108ad9bc8c791eeaba673
17575 <17575@DESKTOP-DVRL4EB>
2025-02-17 23:58:53 -0500

    Add project files.

---
+++ Chapter-2-Projects.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const float RISE_RATE = 1.5;
+
+	cout << "After 5 years, the ocean will rise by " << 5 * RISE_RATE << " mm \n";
+	cout << "After 7 years, the ocean will rise by " << 7 * RISE_RATE << " mm \n";
+	cout << "After 10 years, the ocean will rise by " << 10 * RISE_RATE << " mm";
+}
